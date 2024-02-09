b06b0d1ad92d5d7370a37aeea3e260b089f6ec7d
Nishant ketu <nketu06@gmail.com>
2024-02-10 00:53:54 +0530

    added filters to product

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from typing import Any
-from django.contrib import admin
+from django.contrib import admin,messages
 from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
 from . import models
@@ -9,13 +9,28 @@ from django.urls import reverse
 
 # Register your models here.
 
+class InventoryFilter(admin.SimpleListFilter):
+    title='inventory'
+    parameter_name='inventory'
+
+    def lookups(self, request: Any, model_admin: Any) -> list[tuple[Any, str]]:
+        return [
+            ('<10','Low')
+        ]
+    
+    def queryset(self, request: Any, queryset: QuerySet[Any]) -> QuerySet[Any] | None:
+        if self.value()=='<10':
+            return queryset.filter(inventory__lt=10)
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    actions=['clear_inventory']
     list_display=["title","unit_price","inventory_status","collection__title"]
     list_editable=["unit_price"]
     list_per_page=20
     list_select_related=["collection"]
-    list_filter = ["collection","last_update"]
+    list_filter = ["collection","last_update",InventoryFilter]
 
     def collection__title(self,product):
         return product.collection.title
@@ -25,6 +40,15 @@ class ProductAdmin(admin.ModelAdmin):
         if product.inventory<10:
             return "Low"
         return "Ok"
+    
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self,request,queryset):
+        updated_count=queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f'{updated_count} products were sucessfully updated.',
+            messages.ERROR
+        )
 
 
 
