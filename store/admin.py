6ad4d3a9b9ca4d52fff7230487decf72d7b5dfea
Nishant ketu <nketu06@gmail.com>
2024-02-09 21:01:27 +0530

    registed some models to admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,82 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+from . import models
+from django.db.models.aggregates import Count
+from django.utils.html import format_html,urlencode
+from django.urls import reverse
 
 # Register your models here.
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=["title","unit_price","inventory_status","collection__title"]
+    list_editable=["unit_price"]
+    list_per_page=20
+    list_select_related=["collection"]
+    list_filter = ["collection","last_update"]
+
+    def collection__title(self,product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self,product):
+        if product.inventory<10:
+            return "Low"
+        return "Ok"
+
+
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display=["first_name","last_name","membership","orders"]
+    list_editable=["membership"]
+    list_per_page=20
+    ordering=["first_name","last_name"]
+    search_fields=["first_name__istartswith","last_name__istartswith"]
+
+    @admin.display(ordering='orders_count')
+    def orders(self,customer):
+        url=(reverse('admin:store_order_changelist')
+                    +'?'
+                    + urlencode({
+                        'customer__id': str(customer.id)
+                    }))
+        return format_html('<a href="{}">{} Orders</a>',url,customer.orders_count)
+    
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request).annotate(
+            orders_count=Count('order')
+        )
+    
+
+
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['id',"placed_at",'customer']
+    list_filter=["customer"]
+   
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display=['title','products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self,collection):
+        url=(reverse('admin:store_product_changelist')
+                    +'?'
+                    + urlencode({
+                        'collection__id': str(collection.id)
+                    }))
+        return format_html('<a href="{}">{}</a>',url,collection.products_count)
+    
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
+
